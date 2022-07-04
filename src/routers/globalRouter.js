e78b309b2b77ea3ad639273f6d57239af2a16ccb
kth2642 <kth2642@naver.com>
2022-07-05 00:51:25 +0900

    2022-07-04

---
+++ src/routers/globalRouter.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { home, search } from "../controllers/videoController";
+import { join, login } from "../controllers/userController";
+
+const globalRouter = express.Router();
+
+globalRouter.get("/", home);
+globalRouter.get("/join", join);
+globalRouter.get("/login", login);
+
+export default globalRouter;
---
+++ src/routers/videoRouter.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { upload, watch, getEdit, deleteVideo, postEdit, getUpload, postUpload } from "../controllers/videoController";
+
+const videoRouter = express.Router();
+
+videoRouter.route("/:id([0-9a-f]{24})").get(watch);
+videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
+
+export default videoRouter;
